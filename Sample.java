38acd3f49ed69a9a583813898b1f6bcbada895f7
kpoloju <kpoloju>
2017-03-22 20:42:53 -0700

    added message

--- Sample.java
+++ Sample.java
@@ -2,6 +2,6 @@ class Simple{
    public static void main(String args[]) 
    {
      //Prints abcd"Hello dear m ...JavaPoint"
-	System.out.println("hello mydear@javatpoint");
+	System.out.println("hello @javatpoint");
    }
 }
