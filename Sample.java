04699e5ffb23717fc9d28d1b4fcc72eafd57e05f
kpoloju <kpoloju>
2017-03-24 16:44:04 -0700

    changed content

--- Sample.java
+++ Sample.java
@@ -1,7 +1,38 @@
-class Simple{
-   public static void main(String args[]) 
-   {
-     //Prints abcd"Hello dear m ...JavaPoint"
-	System.out.println("hello hi@dearjavatpoint");
-   }
+class SavingsAccount
+{
+	private double balance;
+	private double interest;
+
+	public SavingsAccount()
+	{
+		balance = 0;
+		interest = 0;
+	}
+
+	public SavingsAccount(double initialBalance, double initialInterest)
+	{
+		balance = initialBalance;
+		interest = initialInterest;
+	}
+
+	public void deposit(double amount)
+	{
+		balance = balance + amount;
+	}
+
+	public void withdraw(double amount)
+	{
+		balance = balance - amount;
+	}
+
+	public void addInterest()
+	{
+		balance = balance + balance * interest;
+	}
+
+	public double getBalance()
+	{
+		return balance;
+	}
+
 }
